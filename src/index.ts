753b9d0b5ac25c00f830f59ecbb7e99190057e79
Tiphaine <tiphainedurand44@gmail.com>
2024-06-28 15:19:13 +0200

    ✨ Add pages transitions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,69 @@
-import { greetUser } from '$utils/greet';
+import barba from '@barba/core'
+import { gsap } from 'gsap'
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin'
 
-window.Webflow ||= [];
+window.Webflow ||= []
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
-});
+  gsap.registerPlugin(ScrollToPlugin)
+
+  barba.init({
+    debug: true,
+    prevent: ({ href }) => {
+      return href.includes('#')
+    },
+    transitions: [
+      {
+        name: 'fade',
+        leave: (data) => {
+          return gsap
+            .to(data.current.container, {
+              duration: 0.5,
+              opacity: 0,
+            })
+            .then(() => {})
+        },
+        enter: (data) => {
+          gsap.from(data.next.container, {
+            opacity: 0,
+            duration: 1,
+          })
+        },
+      },
+    ],
+  })
+
+  function scrollToElement(targetId: string) {
+    const targetElement = document.getElementById(targetId)
+    if (targetElement) {
+      gsap.to(window, {
+        scrollTo: { y: targetElement.offsetTop, autoKill: false },
+        duration: 1,
+        ease: 'sine.inOut',
+      })
+    }
+  }
+
+  const servicesLink = document.getElementById('services-link')
+  if (servicesLink) {
+    servicesLink.addEventListener('click', function (event) {
+      const targetId = 'offers'
+      event.preventDefault()
+
+      if (window.location.pathname === '/') {
+        scrollToElement(targetId)
+      } else {
+        localStorage.setItem('anchor', targetId)
+        barba.go('/')
+      }
+    })
+  }
+
+  barba.hooks.after(() => {
+    const anchor = localStorage.getItem('anchor')
+
+    if (anchor) {
+      scrollToElement(anchor)
+      localStorage.removeItem('anchor')
+    }
+  })
+})
